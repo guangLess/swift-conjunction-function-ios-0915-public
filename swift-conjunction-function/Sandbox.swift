7f5ba420e91e4b4a3550458709dc4d334d268d0e
guangLess <bha.guang@gmail.com>
2015-12-04 10:15:53 -0500

    ya! swiftie is funky musiqe

--- swift-conjunction-function/Sandbox.swift
+++ swift-conjunction-function/Sandbox.swift
@@ -8,3 +8,45 @@ import Foundation
 
 */
 
+//calss object: Equatable _ = {}
+
+
+func listWithElements (var elements:[String] , let conjunction:String) -> String {
+    
+    let countElements = elements.count;
+    var returnString = ""
+    
+    if conjunction == "and" || conjunction == "or" || conjunction == "&" {
+        
+        switch countElements {
+        case 0 :
+            returnString = "";
+        case 1 :
+            returnString = elements[0]
+        case 2 :
+            //conjunction = " and "
+            returnString = "\(elements[0] + " " + conjunction + " " + (elements[1]))"
+        case 3 :
+            //conjunction = " or "
+            
+            let lastString = conjunction + " " + elements[2]
+            elements = [elements[0], elements[1], lastString]
+            returnString = elements.joinWithSeparator(", ") + "."
+        default:
+            var sumString = ""
+            for var i = 0; i < elements.count; i++ {
+                if i == elements.count - 1 {
+                    sumString += "\(conjunction) \(elements[i])"
+                }
+                else {
+                    sumString += "\(elements[i]), "
+                }
+            }
+            returnString = sumString
+        }
+        return returnString
+    }
+    return  "unknown conjunction"
+}
+    
+
